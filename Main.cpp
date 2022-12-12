288d8c773cfe88a304f7a28d3adc9427437d7c4d
Mateus Spencer <mateus@spencer.pt>
2022-12-12 15:46:18 +0000

    Improvements 1400

--- Main.cpp
+++ Main.cpp
@@ -2,41 +2,44 @@
 #include <stdlib.h>
 #include <string>
 #include <unordered_map>
+#include <iostream>
+#include <sstream>
+
 using namespace std;
 
-int find_right_top(int *arr, int size){
+int find_right_top(int *arr, int size, int m){
     int aux_line = 0, aux_right_top = 0, right_top = 0;
 
     for(aux_line = 0; aux_line < size; aux_line++) {
         if(arr[aux_line] > aux_right_top){
             aux_right_top = arr[aux_line];
             right_top = aux_line;
+            if(arr[aux_line] == m)break;
         }
     }
 return right_top;
 }
 
 string board_configuration(int *arr, int size){
-    string configuration = to_string(arr[0]);
-    for(int i = 1; i < size; i++){
-        configuration = configuration + to_string(arr[i]);
+
+    std::ostringstream os;
+    for(int i = 0; i < size; i++){
+        os << arr[i];
     }
-    return configuration;
+    return os.str();
 }
 
-double solve(int *map, std::unordered_map<string, unsigned long long>& solutions,  int n, string configuration){
+unsigned long long solve(int *map, std::unordered_map<string, unsigned long long>& solutions,  int n,int m, string configuration, int right_top){
     unsigned long long total = 0;
-    int right_top= 0, aux_line = 0, max_piece = 0, skip = 0;
+    int aux_line = 0, max_piece = 0, skip = 0, aux_right_top = 0;
     string original_configuration = configuration;;
     int *aux_map = NULL;
-
-    right_top = find_right_top(map,n);
     
     if( map[right_top] <= 1){//ja so é possivel configuração com 1x1s
         return 1;
     }
 
-    if (solutions.find(configuration) != solutions.end()){
+    if (solutions.count(configuration) != 0){
         return solutions[configuration];
     }
     
@@ -57,9 +60,15 @@ double solve(int *map, std::unordered_map<string, unsigned long long>& solutions
                 break;
             }
         }
+
         if(skip == 0){
+            if(piece == max_piece){
+                aux_right_top = find_right_top(aux_map,n, (m-1));
+            }else{
+                aux_right_top = right_top + piece;
+            }
             configuration = board_configuration(aux_map,n);
-            total += solve(aux_map,solutions,n, configuration);
+            total += solve(aux_map,solutions,n,m, configuration, aux_right_top);
         }else{
             skip = 0;
             piece = max_piece+1;
@@ -70,26 +79,16 @@ double solve(int *map, std::unordered_map<string, unsigned long long>& solutions
     return total;
 }
 
-int input(){
-    int invalido = 1, val = 0;
-    while(invalido){
-        if(scanf("%d", &val) == 1){
-            invalido = 0;
-        }else{
-            scanf("%*s");
-        }
-    }
-    return val;
-}
-
 int main(int argc, char *argv[]){
-    int n = 0;
+    int n = 0,m = 0, j = 0;
     unsigned long long sol = 0;
     int *map = NULL;
     std::unordered_map<string, unsigned long long> solutions;
 
-    n = input();
-    input();
+    cin >> j;
+    n = j;
+    cin >> j;
+    m = j;
 
     map = (int*)calloc(n,sizeof(int));
     if(map == NULL){
@@ -98,17 +97,19 @@ int main(int argc, char *argv[]){
     }
 
     for(int i = 0; i<n; i++){
-        map[i] = input();
+        cin >> j;
+        map[i] = j;
     }
 
-    int right_top = find_right_top(map,n);
+    int right_top = find_right_top(map,n,m);
     
     if( map[right_top] != 0){ //mapa vazio
         string configuration = board_configuration(map,n);
-        sol = solve(map,solutions,n,configuration);
+        sol = solve(map,solutions,n,m,configuration, right_top);
     }
     
-    printf("\n%llu",sol);
+    cout << sol << endl;
+
     free(map);
-    return 1;    
+    return 0;    
 }
