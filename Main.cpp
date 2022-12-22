190e90e1aa2fdc944ddb111b1b490fd4009dd801
Mateus Spencer <mateus@spencer.pt>
2022-12-22 23:10:10 +0000

    Last Submission

--- Main.cpp
+++ Main.cpp
@@ -1,57 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <vector>
 #include <unordered_map>
 #include <iostream>
 #include <sstream>
 
 using namespace std;
 
-int find_right_top(int *arr, int size, int m){
-    int aux_line = 0, aux_right_top = 0, right_top = 0;
+int find_right_top(vector<int> vect,  int m){
+    int aux_line = 0, aux_right_top = 0, right_top = 0, size = vect.size();
 
     for(aux_line = 0; aux_line < size; aux_line++) {
-        if(arr[aux_line] > aux_right_top){
-            aux_right_top = arr[aux_line];
+        if(vect[aux_line] > aux_right_top){
+            aux_right_top = vect[aux_line];
             right_top = aux_line;
-            if(arr[aux_line] == m)break;
+            if(vect[aux_line] == m)break;
         }
     }
 return right_top;
 }
 
-string board_configuration(int *arr, int size){
-
-    std::ostringstream os;
-    for(int i = 0; i < size; i++){
-        os << arr[i];
+size_t hash_func(std::vector<int>& vec) {
+    std::size_t seed = vec.size();
+    for(auto x : vec) {
+        x = ((x >> 16) ^ x) * 0x45d9f3b;
+        x = ((x >> 16) ^ x) * 0x45d9f3b;
+        x = (x >> 16) ^ x;
+        seed ^= x + 0x9e3779b9 + (seed << 6) + (seed >> 2);
     }
-    return os.str();
+    return seed;
 }
 
-unsigned long long solve(int *map, std::unordered_map<string, unsigned long long>& solutions,  int n,int m, string configuration, int right_top){
+unsigned long long solve(vector<int> map, std::unordered_map<uint64_t, unsigned long long>& solutions, int m, int right_top){
     unsigned long long total = 0;
-    int aux_line = 0, max_piece = 0, skip = 0, aux_right_top = 0;
-    string original_configuration = configuration;;
-    int *aux_map = NULL;
+    int aux_line = 0, max_piece = 0, skip = 0, aux_right_top = 0, size = map.size();
+    uint64_t configuration = 0;
     
     if( map[right_top] <= 1){//ja so é possivel configuração com 1x1s
         return 1;
     }
-
+    configuration = hash_func(map);
     if (solutions.count(configuration) != 0){
         return solutions[configuration];
     }
     
-    for(aux_line = right_top; aux_line <n; aux_line++){ // ate que tamanho de peça posso colocar
+    for(aux_line = right_top; aux_line <size; aux_line++){ // ate que tamanho de peça posso colocar
         if( map[aux_line] < map[right_top] ) break;
         max_piece ++;
     }
 
-    aux_map = (int*)calloc(n,sizeof(int));//novo mapa para passar recursivamente
+    vector<int> aux_map;
     for(int piece = 1;piece <= max_piece; piece++){
-        for(aux_line = 0; aux_line < n; aux_line ++){//copy map to aux_map
-            aux_map[aux_line] = map[aux_line];
+        aux_map.clear();
+        for(aux_line = 0; aux_line < size; aux_line ++){//copy map to aux_map
+            aux_map.push_back(map[aux_line]);
         }
         for(aux_line = right_top; aux_line < (right_top + piece); aux_line++){ //retirar peça do mapa
             aux_map[aux_line] = aux_map[aux_line] - piece;
@@ -60,56 +63,46 @@ unsigned long long solve(int *map, std::unordered_map<string, unsigned long long
                 break;
             }
         }
-
         if(skip == 0){
             if(piece == max_piece){
-                aux_right_top = find_right_top(aux_map,n, (m-1));
+                aux_right_top = find_right_top(aux_map, (m-1));
             }else{
                 aux_right_top = right_top + piece;
             }
-            configuration = board_configuration(aux_map,n);
-            total += solve(aux_map,solutions,n,m, configuration, aux_right_top);
+            total += solve(aux_map,solutions,m, aux_right_top);
         }else{
             skip = 0;
             piece = max_piece+1;
         } 
     }
-    solutions[original_configuration] = total;
-    free(aux_map);
+    solutions[configuration] = total;
     return total;
 }
 
-int main(int argc, char *argv[]){
-    int n = 0,m = 0, j = 0;
+int main(){
+    int n = 0,m = 0, j = 0, first = 1;
     unsigned long long sol = 0;
-    int *map = NULL;
-    std::unordered_map<string, unsigned long long> solutions;
+    vector<int> map;
+    std::unordered_map<uint64_t, unsigned long long> solutions;
 
     cin >> j;
     n = j;
     cin >> j;
     m = j;
 
-    map = (int*)calloc(n,sizeof(int));
-    if(map == NULL){
-        fprintf (stderr, "Error: not enough memory available");
-        exit(0);
-    }
-
     for(int i = 0; i<n; i++){
         cin >> j;
-        map[i] = j;
+        if(first != 1 ||  j != 0 ){
+            first = 0;
+            map.push_back(j);
+        }
     }
-
-    int right_top = find_right_top(map,n,m);
     
-    if( map[right_top] != 0){ //mapa vazio
-        string configuration = board_configuration(map,n);
-        sol = solve(map,solutions,n,m,configuration, right_top);
+    if( !map.empty()){ //mapa vazio
+        int right_top = find_right_top(map,m);
+        sol = solve(map,solutions,m, right_top);
     }
     
     cout << sol << endl;
-
-    free(map);
     return 0;    
 }
